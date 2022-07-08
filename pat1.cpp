84e9cab1aead0183b26824fd742690e699ea264e
SIDDHHARTH <79316753+siddharth7901@users.noreply.github.com>
2022-07-08 17:14:37 +0530

    Add files via upload

---
+++ pat1.cpp
@@ -0,0 +1,80 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+
+//     int rows, coloumns;
+//     cout << "Enter the rows " << endl;
+//     cin >> rows;
+//     cout << "Enter the coloumns " << endl;
+//     cin >> coloumns;
+
+//     for (int i = 1; i <= rows; i++)
+//     {
+//         for (int j = 1; j <= coloumns; j++)
+//         {
+//             cout << " * ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// // output
+// Enter the rows 
+// 3
+// Enter the coloumns 
+// 4
+//  *  *  *  * 
+//  *  *  *  *
+//  *  *  *  *
+
+
+
+// print a hollow rectangle 
+// Language: cpp
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int rows, coloumns;
+//     cout << "Enter the rows " << endl;
+//     cin >> rows;
+//     cout << "Enter the coloumns " << endl;
+//     cin >> coloumns;
+
+//     for (int i =1; i<=rows; i++){
+//         for(int j=1;j<=coloumns;j++){
+//             if(i==1 || i==rows || j==1 || j==coloumns){
+//                 cout << " * ";
+//             }
+//             else{
+//                 cout << "   ";
+//             }
+//         }
+//         cout << endl;
+
+//     }
+
+// return 0;
+// }
+
+// output
+// Enter the rows 
+// 5
+// Enter the coloumns 
+// 5
+//  *  *  *  *  *
+//  *           *
+//  *           *
+//  *           *
+// //  *  *  *  *  *
+
+
+// print a half pyramid rectangle
+
